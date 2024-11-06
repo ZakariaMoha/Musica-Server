db95a46c8a60bf3c938a1460ffa158dd05a57786
Zakaria <zakmi.moha@gmail.com>
2024-11-06 07:28:22 +0300

    Album model

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 #  User Table
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Interger, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,6 +17,7 @@ class User(db.Model):
 
 # Artist Table
 class Artist(db.Model):
+    __tablename__ = 'artists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     bio = db.Column(db.Text)
@@ -24,6 +26,16 @@ class Artist(db.Model):
     albums = db.relationship('Album', backref='artist', lazy=True)
     # One to many relationship, An artist can be associated with multiple genres. Table artist related to genre table
     genres = db.relationship('Genre', secondary='artist_genre', backref=db.backref('artists', lazy='dynamic'))
+
+# Album Table
+class Album(db.Model):
+    __tablename__ = 'albums'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+
+    # Many to many relationship, Album can contain multiple songs, Table album related to songs table
+    songs = db.relationship('Song', secondary='album_songs', backref=db.backref('albums', lazy='dynamic'))
 
 
 

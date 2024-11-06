cb20f7a5f2a712f3f77eecc1663561a1bbea7712
Zakaria <zakmi.moha@gmail.com>
2024-11-06 07:17:05 +0300

    Artist Model

--- models.py
+++ models.py
@@ -11,5 +11,22 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
-    # One to many relationship, User can create multiple playlists
+    # One to many relationship, User can create multiple playlists, Table user related to playlist table 
     playlists = db.relationship('Playlist', backref='user', lazy=True)
+
+# Artist Table
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    bio = db.Column(db.Text)
+
+    # One to many relationship, Artist can create multiple albums, Table artist related to albums table
+    albums = db.relationship('Album', backref='artist', lazy=True)
+    # One to many relationship, An artist can be associated with multiple genres. Table artist related to genre table
+    genres = db.relationship('Genre', secondary='artist_genre', backref=db.backref('artists', lazy='dynamic'))
+
+
+
+
+
+    
